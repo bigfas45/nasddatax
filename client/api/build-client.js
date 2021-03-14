77f143430bea1e2a817cfcc59a8bd0712b1c4dcd
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-03-14 21:06:50 +0100

    uploading backend dev

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,9 +5,9 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      // baseURL: 'http://www.nasdfundy.com/',
+      // baseURL:
+      //   'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.nasdfundy.com/',
 
       headers: req.headers,
     });
