91e0768fc367c77e829c25cc3a8b5e14065a9275
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-04-12 17:25:17 +0100

    Mysql connection end issues

--- brokers/src/app.ts
+++ brokers/src/app.ts
@@ -17,11 +17,8 @@ import { DealingMemberGetRouter } from './routes/get-dealing-member';
 import { MyBuyRangeRouter } from './routes/my-buy-tradeRange';
 import { MySellRangeRouter } from './routes/my-sell-trade-range';
 
-import { IndexConnectRouter } from './routes/connect-care';
 
-import { IndexEmail1Router} from './routes/email-connect-1';
 
-import { UserUpdateRouter} from './routes/update';
 
 
 // import {TradeLogRouter} from './routes/trade-log'
@@ -51,9 +48,6 @@ app.use(DealingMemberAddRouter);
 app.use(DealingMemberGetRouter);
 app.use(MyBuyRangeRouter);
 app.use(MySellRangeRouter);
-app.use(IndexConnectRouter);
-app.use(IndexEmail1Router);
-app.use(UserUpdateRouter);
 
 
 app.get('*', async (req, res ) => {
