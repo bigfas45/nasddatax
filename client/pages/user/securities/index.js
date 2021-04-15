7fddf7c21acd549ff1605dee8a89c261aea1a80e
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-04-15 19:09:06 +0100

    update

--- client/pages/user/securities/index.js
+++ client/pages/user/securities/index.js
@@ -127,7 +127,6 @@ const Securities = ({ currentUser }) => {
   });
 
   useEffect(() => {
-    
     currentUser && currentUser.status === 'free'
       ? Router.push('/auth/access-denied')
       : '';
