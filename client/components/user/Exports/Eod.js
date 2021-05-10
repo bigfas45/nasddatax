339baeab00b4c8ba94ac5e147cbb4f64277861fe
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-05-10 19:08:03 +0100

    fixes

--- client/components/user/Exports/Eod.js
+++ client/components/user/Exports/Eod.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect, useState, Component } from "react";
 import ReactHTMLTableToExcel from 'react-html-table-to-excel'
+import InvestorName from '../../components/admin/investorName';
 
 const ExportToExcel = ({post}) => {
 
@@ -20,9 +21,11 @@ const ExportToExcel = ({post}) => {
           <tr>
             <th>EXTERNAL_TICKET</th>
             <th>TO_MEMBER</th>
+            <th>Account Name</th>
             <th>TO_ACCOUNT</th>
             <th>TO_REFERENCE</th>
             <th>FROM_MEMBER</th>
+            <th>Account Name</th>
             <th>FROM_ACCOUNT</th>
             <th>FROM_REFERENCE</th>
             <th>SYMBOL</th>
@@ -42,9 +45,17 @@ const ExportToExcel = ({post}) => {
               <tr key={i}>
                 <td> {r.EXTERNAL_TICKET} </td>
                 <td> {r.TO_MEMBER}</td>
+                <td>
+                  {' '}
+                  <InvestorName account={r.TO_ACCOUNT} />
+                </td>
                 <td> {r.TO_ACCOUNT}</td>
                 <td> {r.TO_REFERENCE}</td>
                 <td> {r.FROM_MEMBER}</td>
+                <td>
+                  {' '}
+                  <InvestorName account={r.FROM_ACCOUNT} />
+                </td>
                 <td> {r.FROM_ACCOUNT}</td>
                 <td> {r.FROM_REFERENCE}</td>
                 <td> {r.SYMBOL}</td>
