74f09c9f219b45f8c5e64a686867f1d53293af64
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-05-27 06:34:20 +0100

    mysql fixes

--- securities/src/models/mysql.ts
+++ securities/src/models/mysql.ts
@@ -1,14 +1,15 @@
 import mysql from 'mysql';
+
 const db = mysql.createPool({
     host      : 'nasdotcng.com',
     user      : 'notcsadm_otc',
     password  : 'N#M3IM^stG0' ,
     database  :  'notcsadm_nplcsadm_nsd_new',
-    multipleStatements: true
+    connectionLimit: 5,
   });
   
-  // connect 
-  // @ts-ignore
+  
+  
 db.getConnection(function(err, connection) {
   // connected! (unless `err` is set)
   connection.end();
