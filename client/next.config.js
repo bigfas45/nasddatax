d85ba4e0c86d352152d9494d4a67bb4bb602ef77
NASD OTC <38135973+bigfas45@users.noreply.github.com>
2021-06-20 10:24:28 +0100

    production

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-    webpackDevMiddleware:config => {
-        config.watchOptions.poll = 300;
-        return config;
-    }
-}
+  webpackDevMiddleware: (config) => {
+    config.watchOptions.poll = 300;
+    return config;
+  },
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
 
 const withCSS = require('@zeit/next-css');
